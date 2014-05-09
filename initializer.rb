a8997ff38d0bac54fc28d01124f712e6bdee45f7
Boris <boris.bbk@gmail.com>
2014-05-09 14:06:12 +0300

    avoid $oid

--- initializer.rb
+++ initializer.rb
@@ -3,6 +3,15 @@ require 'sinatra'
 require 'sinatra/json'
 
 ENV['MONGOID_ENV'] = ENV['RACK_ENV'] || 'development'
+
+module BSON
+  class ObjectId
+    def as_json(options = {})
+      to_s
+    end
+  end
+end
+
 Mongoid.load!('./config/mongoid.yml')
 Dir.glob(File.join('./app/models','*.rb')).each {|f| require f}
 Dir.glob(File.join('./app/services','*.rb')).each {|f| require f}
